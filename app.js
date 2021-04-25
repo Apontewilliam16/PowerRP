5309d187ba47059ef6abc3721623611bff27c2e7
apontewilliam16 <apontewilliam16@gmail.com>
2021-04-25 13:40:57 -0400

    major changes to about page

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get("/contact", function (req, res) {
   });
 });
 
+app.get("/rules", function(req, res){
+  res.render("rules", {
+
+  });
+});
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
